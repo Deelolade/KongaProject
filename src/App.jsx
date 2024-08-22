527b9d5a86e8df4e8c900c8fb9edb4fb11ebab62
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-08-22 18:14:16 +0100

    Login page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import LoginPage from './pages/LoginPage'
 const App = () => {
   return (
     <div>
+      <LoginPage/>
       <SignUp/>
       <Home/>
     </div>
