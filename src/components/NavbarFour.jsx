017277279b043f120d3ac598ee9c37fc73990274
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-24 18:44:19 +0100

    debugging-16

--- src/components/NavbarFour.jsx
+++ src/components/NavbarFour.jsx
@@ -8,34 +8,34 @@ import WatchKongaTvNav from '../Images/WatchKongaTvNav.webp'
 const NavbarFour = () => {
   return (
     <div>
-      <div className='container-fluid mt-5 mt-md-2 mt-lg-5'>
-        <div className='row text-center'>
-            <div className='col-xl-2 col-md-2'>
+      <div className='container-fluid mt-2 mt-md-2 mt-lg-5'>
+        <div className='row text-center mx-auto'>
+            <div className='col-xl-2 col-md-2 col-4'>
                 <div className='navbarFour shadow-sm py-2 rounded border-light border-opacity-75' role="button">
                 <img src={KongaBlogNav} alt="" className='w-100'/>
                 </div>
             </div>
-            <div className='col-xl-2 col-md-2'>
+            <div className='col-xl-2 col-md-2 col-4'>
                 <div className='navbarFour shadow-sm py-2 rounded border-light border-opacity-75' role="button">
                 <img src={PayBillsNav} alt="" className='w-100'/>
                 </div>
             </div>
-            <div className='col-xl-2 col-md-2'>
+            <div className='col-xl-2 col-md-2 col-4'>
                 <div className='navbarFour shadow-sm py-2 rounded border-light border-opacity-75' role="button">
                 <img src={OfflineStoreNav} alt="" className='w-100'/>
                 </div>
             </div>
-            <div className='col-xl-2 col-md-2'>
+            <div className='col-xl-2 col-md-2 col-4'>
                 <div className='navbarFour shadow-sm py-2 rounded border-light border-opacity-75' role="button">
                 <img src={BrandStoresNav} alt="" className='w-100'/>
                 </div>
             </div>
-            <div className='col-xl-2 col-md-2'>
+            <div className='col-xl-2 col-md-2 col-4'>
                 <div className='navbarFour shadow-sm py-2 rounded border-light border-opacity-75' role="button">
                 <img src={BookFlightsNav} alt="" className='w-100'/>
                 </div>
             </div>
-            <div className='col-xl-2 col-md-2'>
+            <div className='col-xl-2 col-md-2 col-4'>
                 <div className='navbarFour shadow-sm py-2 rounded border-light border-opacity-75' role="button">
                 <img src={WatchKongaTvNav} alt="" className='w-100'/>
                 </div>
