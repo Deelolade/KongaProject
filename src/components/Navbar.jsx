40b5d068e1ad2818a67e7f78b5c7f9daec9cb3fd
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-24 03:16:38 +0100

    debugging-14

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
   return (
     <div className=' '>
       <nav className="navbar navbar-expand">
-        <div className=" d-flex justify-content-center align-items-center mx-auto">
-          <Link to='/'><img src={KongaLogo} alt="" className='me-5' /></Link>
+        <div className=" d-flex justify-content-center align-items-center mx-auto main-nav">
+          <Link to='/'><img src={KongaLogo} alt="" className='me-5 Logo' /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -36,7 +36,7 @@ const Navbar = () => {
               <li className="nav-item">
                 <Link to='/sellonkonga' className="nav-link " >Sell on Konga</Link>
               </li>
-            </ul> <form className="d-flex mx-3 justify-content-center align-items-center" role="search" onSubmit={handleSubmit}>
+            </ul> <form className="d-flex mx-lg-3 mx-md-1 justify-content-center align-items-center" role="search" onSubmit={handleSubmit}>
               <input className="py-3" type="search" placeholder="Search for products,brands and categories..." aria-label="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
 
               <button type="submit" className='d-flex py-3 justify-content-center align-items-center'><AiOutlineSearch className='' /> </button>
@@ -57,19 +57,21 @@ const Navbar = () => {
               </div>
 
               <li className="nav-item">
-                <Link to='/login' className="nav-link " aria-current="page">Login/SignUp</Link>
+                <Link to='/login' className="nav-link " aria-current="page">Login / SignUp</Link>
               </li>
             </ul>
 
-            <span className='cart-icon' style={{width:"auto",height:"auto",fontSize:"15px"}}><Link to="/shoppingcart" className="nav-link d-flex align-items-center  justify-content-center rounded-1 cart-icon py-2 px-2" style={{ backgroundColor: '#2D9E6D', color: 'white'}}>
+            <span className='cart-icon' style={{ width: "auto", height: "auto", fontSize: "15px" }}>
+              <Link to="/shoppingcart" className="nav-link d-flex align-items-center  justify-content-center rounded-1 cart-icon py-2 px-2" style={{ backgroundColor: '#2D9E6D', color: 'white' }}>
                 <FaShoppingCart size={20} />
-              <span className="ms-1" style={{fontSize:"15px"}}>
-                My Cart
-              </span>
-              <span className="badge bg-white ms-2" style={{ fontSize: '0.9rem', color: 'black' }}>
-                {getTotalItems()}
-              </span>
-            </Link></span>
+                <span className="ms-1" style={{ fontSize: "15px" }}>
+                  My Cart
+                </span>
+                <span className="badge bg-white ms-2" style={{ fontSize: '0.9rem', color: 'black' }}>
+                  {getTotalItems()}
+                </span>
+              </Link>
+            </span>
           </div>
         </div>
       </nav>
